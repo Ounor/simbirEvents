2a9569e83347580f975f82348fd7328d40e9f60f
a_pereladov <aleksandr.pereladov@simbirsoft.com>
2022-09-11 02:34:18 +0500

    test

--- queries.js
+++ queries.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 const { Pool } = require('pg')
 const connectionString = 'postgres://ulgpbzcaxnbkft:be2cfa564ede47fff1b20578c0df3b8426edec6086da33da15db2b7f7b898f0d@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d1ebnbl3rm8869'
 
@@ -43,7 +44,7 @@ const createFeedback = (request, response) => {
 }
 
 const getAllParticipants = (request, response) => {
-    const {eventId} = request.body
+    const {eventId} = request.query
     pool.query('SELECT simbirsoft.getallparticipants($1)', [eventId] ,(error, results) => {
         if (error) {
             response.status(500).json(error.message)
@@ -100,6 +101,18 @@ const getEventsbyId = (request, response) => {
       })
 }
 
+const createEvent = (request, response) => {
+  const {name, date, cityId, place, description, maxCount, direction, eventHolderId} = request.body
+    const thumbnail = request.file.filename || ''
+    pool.query('SELECT simbirsoft.createevent($1, $2::timestamp without time zone, $3, $4, $5, $6, $7, $8, $9)',[name, date, cityId, place, description, maxCount, direction, thumbnail, eventHolderId], (error, results) => {
+          if (error) {
+              response.status(500).json(error.message)
+              return
+
+          }
+          response.status(200).json(results.rows)
+      })
+}
 
 module.exports = {
   addUserToEvent,
@@ -109,5 +122,6 @@ module.exports = {
   getCity,
   getEvents,
   getDirection,
-  getEventsbyId
+  getEventsbyId,
+  createEvent
 }
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const fs = require('fs');
 const db = require('./queries')
+const multer = require('multer')
 const bodyParser = require("body-parser");
 // import swaggerAutogen from 'swagger-autogen'
 // const endpointsFiles = [join(_dirname, './queries.js')]
@@ -13,12 +15,40 @@ app.use(
     })
 )
 
-//  app.use(express.methodOverride());
-app.use(bodyParser.multipart({
-    uploadDir: './uploads',
-    keepExtensions: true
-}));
+
+const storage  = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let newDestination = ''
+        if (req.body.eventId) {
+            newDestination = `uploads/${req.body.eventId}`;
+        } else {
+            newDestination = `uploads/`;
+        }
+        const stat = null;
+        try {
+            stat = fs.statSync(newDestination);
+        } catch (err) {
+            fs.mkdirSync(newDestination);
+        }
+        if (stat && !stat.isDirectory()) {
+            throw new Error('Directory cannot be created because an inode of a different type exists at "' + dest + '"');
+        }       
+        cb(null, newDestination);
+    }
+});
+
+const upload = multer(
+    { 
+        dest: 'uploads/',
+        limits: {
+            fieldNameSize: 100,
+            fileSize: 60000000
+        },
+        storage: storage
+    }
+);
  
+
 app.get('/getCity/', db.getCity)
 //
 app.get('/getEvents/', db.getEvents) 
@@ -34,14 +64,13 @@ app.get('/getDirection/', db.getDirection)
 //
 app.get('/getEventsbyId/', db.getEventsbyId) 
 //
-app.post('/upload', function(req, res){
-    // Returns json of uploaded file
-    res.json(req.files);
-});
-
+app.post("/uploadFiles", upload.array("files"), () =>  res.json({ message: "Successfully uploaded files" }));
+ 
+app.post('/createEvent/',  upload.single('thumbnail'), db.createEvent
+) 
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+    console.log(`App running.`)
 })
 
  
